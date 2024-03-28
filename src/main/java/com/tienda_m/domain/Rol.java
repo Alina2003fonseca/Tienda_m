76dda071c6fb974ec86abfc8e15b4d26ebd82595
alina <alina@192.168.1.2>
2024-03-27 22:22:10 -0600

    Semana 10 Seguridad

---
+++ src/main/java/com/tienda_m/domain/Rol.java
@@ -0,0 +1,25 @@
+
+package com.tienda_m.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="rol")
+public class Rol implements Serializable{
+   
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_rol")
+    private Long idRol;
+    
+    private String nombre;
+    
+    @Column(name="id_usuario")
+    private Long idUsuario;
+    
+    
+}
